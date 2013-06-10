67019b4271519bc3f7098d3845dceb5ea5f927cb
Christian Vaas <christianvaas@auspex.eu>
2013-06-10 16:05:47 +0200

    Added option -i for inplace transpilation i.e. source and destination files are equal

--- assets/transpiler/transpiler-cli.js
+++ assets/transpiler/transpiler-cli.js
@@ -12,11 +12,16 @@ var tracing = require("./transpiler-lib.js").tracing;
 var fs = require("fs");
 
 /*  command-line arguments  */
-var filename_src = process.argv[2];
+var filename_src
 var filename_dst = process.argv[3];
+if (process.argv[2] !== '-i') {
+	filename_src = process.argv[2];
+} else {
+	filename_src = filename_dst;
+}
 
 /*  read original source, instrument it and write instrumented source  */
 var source = fs.readFileSync(filename_src, { encoding: "utf8" });
-source = tracing.instrument("ComponentJS", source);
+source = tracing.instrument("cs", source);
 fs.writeFileSync(filename_dst, source, { encoding: "utf8" });
 
