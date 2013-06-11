1e6e6fe4643d9899ff94cc22632ae93638411cb3
Ralf S. Engelschall <rse@engelschall.com>
2013-06-11 22:09:05 +0200

    upgrade to latest jQuery 2.0.2

--- app/sv/root.js
+++ app/sv/root.js
@@ -1,6 +1,10 @@
 /*
-**  root.js -- Root Service
-**  Copyright (c) 2013 Ralf S. Engelschall <rse@engelschall.com>
+**  ComponentJS -- Component System for JavaScript <http://componentjs.com>
+**  Copyright (c) 2009-2013 Ralf S. Engelschall <http://engelschall.com>
+**
+**  This Source Code Form is subject to the terms of the Mozilla Public
+**  License, v. 2.0. If a copy of the MPL was not distributed with this
+**  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
 /* jshint unused:false*/
