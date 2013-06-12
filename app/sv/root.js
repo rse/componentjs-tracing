3b10c950c24ec12912d47688b18737b8b1213b01
Christian Vaas <christianvaas@auspex.eu>
2013-06-12 14:12:13 +0200

    Improved robustness, a connecting client joins now automatically to the tracing room. This also happens on reconnect

--- app/sv/root.js
+++ app/sv/root.js
@@ -7,34 +7,33 @@
 **  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-/* jshint unused:false*/
-
 module.paths = module.paths.concat(module.parent.paths);
 
-var path       = require("path");
-var util       = require("util");
-var express    = require("express");
-var express_io = require("express.io");
+var express    = require('express')
+var express_io = require('express.io')
 
 module.exports = {
     setup: function (ctx) {
-        var srv = express_io();
-        srv.http().io();
-        srv.disable("x-powered-by");
+        var srv = express_io()
+        srv.http().io()
+        srv.disable('x-powered-by')
 
-        srv.use(express.bodyParser());
-        srv.use(express.methodOverride());
+        srv.use(express.bodyParser())
+        srv.use(express.methodOverride())
 
-        ctx.srv.io.route('join', function (req, res) {
-            req.io.join('tracingRoom')
-            console.log('joining tracingRoom')
+        ctx.srv.io.on('connection', function (socket) {
+            console.log('New inbound connection')
+            socket.on('disconnect', function () {
+                console.log('Client disconnected')
+            })
+            console.log('Joining tracing queue')
+            socket.join('tracingRoom')
         })
 
-        ctx.srv.io.route('trace', function (req, res) {
-            req.io.room('tracingRoom').broadcast('newTrace', req.data)
+        ctx.srv.io.route('trace', function (req) {
+            ctx.srv.io.sockets.in('tracingRoom').emit('newTrace', req.data)
         })
 
-        return srv;
+        return srv
     }
-};
-
+}
