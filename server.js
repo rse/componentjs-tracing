035d4860ee6d6f9c30a93fc0579ae2a76596f1ab
Christian Vaas <christianvaas@auspex.eu>
2013-06-13 10:05:54 +0200

    Added documentation and cleaned up code

--- server.js
+++ server.js
@@ -277,71 +277,65 @@ proxyserver.on("http-error", function (cid, error) {
    app.logger.log("info", "proxy: " + cid + ": HTTP error: " + error);
 });
 
+/*  get supplied arguments, identifying the ComponentJS library file and the application component files  */
 var cjsFile  = new RegExp(opts.componentjs)
 var cmpFiles = new RegExp(opts.components)
 
 proxyserver.on("http-intercept-response", function (cid, request, response, remoteResponse, remoteResponseBody, performResponse) {
-    var buffer
-    var injected = false
+    /*  Did we inject anything? If yes, fix the HTTP request  */
+    var finishResponse = function () {
+        /*  Make sure the file is never marked as loaded from cache  */
+        remoteResponse.statusCode = 200;
+        remoteResponse.headers["content-length"] = remoteResponseBody.length;
+        remoteResponse.headers["content-type"] = "application/javascript";
+        remoteResponse.headers["accept-ranges"] = "bytes";
+        /*  Take care of the caching headers  */
+        var cacheControl = remoteResponse.headers["x-cache"];
+        if (cacheControl)
+            cacheControl = cacheControl.replace("HIT", "MISS");
+        cacheControl = remoteResponse.headers["x-cache-lookup"];
+        if (cacheControl)
+            cacheControl = cacheControl.replace("HIT", "MISS");
+    };
+
     if (remoteResponse.req.path.match(cjsFile) !== null) {
-        console.log('Discovered CJS file: ' + request.url)
-        /*  Inject the given files  */
+        /*  Convert the remoteResponseBody to a string  */
+        remoteResponseBody = remoteResponseBody.toString("utf8");
+        console.log("Discovered CJS file: " + request.url);
+
+        /*  Which files do we want to be injected?  */
         var filesToInject = [
-            './assets/plugins/component.plugin.tracing.js',
-            './assets/socket.io.js',
-            './assets/plugins/component.plugin.tracing-remote.js'
-        ]
+            "./assets/plugins/component.plugin.tracing.js",
+            "./assets/socket.io.js",
+            "./assets/plugins/component.plugin.tracing-remote.js"
+        ];
 
-        /*  Load the original file to a temporary buffer  */
+        /*  Should the latest version of ComponentJS be injected as well?  */
         if (opts.latestcjs) {
-            console.log('Injecting the latest ComponentJS version')
-            filesToInject.unshift('./assets/component.js')
-            buffer = new Buffer(0)
-        } else {
-            buffer = new Buffer(remoteResponseBody, 'utf8')
+            console.log("Injecting the latest ComponentJS version");
+            filesToInject.unshift("./assets/component.js");
+            remoteResponseBody = "";
         }
 
+        /*  Now inject each file */
         for (var i = 0; i < filesToInject.length; i++) {
-            /*  Load the file to inject to a temporary buffer  */
-            var append = fs.readFileSync(filesToInject[i])
-            var tmpBuffer = new Buffer(buffer.length + append.length)
-
-            /*  Copy over the already present buffer  */
-            buffer.copy(tmpBuffer)
-            /*  Append the current file  */
-            append.copy(tmpBuffer, buffer.length)
-            /*  Reassign the buffers for the next iteration  */
-            buffer = tmpBuffer
+            /*  Load the file to inject to a temporary string  */
+            var append = fs.readFileSync(filesToInject[i]).toString("utf8");
+            /*  Append file to the responseBody  */
+            remoteResponseBody += append;
         }
 
-        remoteResponseBody = buffer.toString('utf8')
-        console.log('Append necessary plug-ins and libraries')
-        injected = true
+        console.log("Append necessary plug-ins and libraries");
+        finishResponse();
     } else if (remoteResponse.req.path.match(cmpFiles) !== null) {
         /*  read original remoteResponseBody, instrument it and write instrumented remoteResponseBody  */
         remoteResponseBody = remoteResponseBody.toString("utf8");
-        remoteResponseBody = tracing.instrument('cs', remoteResponseBody);
-        buffer = { length: remoteResponseBody.length }
+        remoteResponseBody = tracing.instrument("cs", remoteResponseBody);
 
-        console.log('Transpiled component file: ' + request.url)
-        injected = true
+        console.log("Transpiled component file: " + request.url);
+        finishResponse();
     }
-    /*  Did we inject anything? If yes, fix the HTTP properties  */
-    if (injected) {
-        var length = buffer.length
-        /*  Make sure the file is never loaded from cache  */
-        remoteResponse.statusCode = 200
-        remoteResponse.headers['content-length'] = length
-        remoteResponse.headers['content-type'] = 'application/javascript'
-        remoteResponse.headers['accept-ranges'] = 'bytes'
-        /*  Take care of the caching headers  */
-        var cacheControl = remoteResponse.headers['x-cache']
-        if (cacheControl)
-            cacheControl = cacheControl.replace('HIT', 'MISS')
-        cacheControl = remoteResponse.headers['x-cache-lookup']
-        if (cacheControl)
-            cacheControl = cacheControl.replace('HIT', 'MISS')
-    }
+
     performResponse(remoteResponse, remoteResponseBody);
 });
 
