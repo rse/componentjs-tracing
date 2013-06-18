40a0d2233284f68f25522348544e8f5669c40dbb
Christian Vaas <christianvaas@auspex.eu>
2013-06-18 13:21:28 +0200

    Added new component for headline

---
+++ app/ui/app/app-ui-widget-headline.js
@@ -0,0 +1,21 @@
+/*
+**  ComponentJS -- Component System for JavaScript <http://componentjs.com>
+**  Copyright (c) 2009-2013 Ralf S. Engelschall <http://engelschall.com>
+**
+**  This Source Code Form is subject to the terms of the Mozilla Public
+**  License, v. 2.0. If a copy of the MPL was not distributed with this
+**  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+cs.ns("app.ui.widget.headline")
+
+/*  widget view  */
+app.ui.widget.headline.view = cs.clazz({
+    mixin: [ cs.marker.view ],
+    protos: {
+        render: function () {
+            var ui = $.markup("widget-headline")
+            cs(this).plug(ui)
+        }
+    }
+})
