cda93af1a812973c20eeb58cc4a015bb820aff78
Ralf S. Engelschall <rse@engelschall.com>
2013-06-19 15:19:47 +0200

    bump version before release

--- componentjs-tracing.js
+++ componentjs-tracing.js
@@ -15,7 +15,7 @@
 /*  program identification  */
 var app = {
     name: "ComponentJS Tracing",
-    vers: "0.9.0",
+    vers: "0.9.1",
     date: "2013-06-19",
     logger: null,
     basedir: null
