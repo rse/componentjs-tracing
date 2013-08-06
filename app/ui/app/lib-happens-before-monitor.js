1e4eba75b2490d00ca04aeb367603096effca7fe
Christian Vaas <christianvaas@auspex.eu>
2013-08-06 09:57:15 +0200

    Removed trace condition evalutation code

--- app/ui/app/lib-happens-before-monitor.js
+++ app/ui/app/lib-happens-before-monitor.js
@@ -9,53 +9,6 @@
 
 (function () {
 
-var evaluateExpr = function (ctx, expression, binding) {
-    var type = expression.type
-
-    if (type === 'true')
-        return true
-    else if (type === 'false')
-        return false
-    else if (type === 'and')
-        return evaluateExpr(ctx, expression.left, binding) && evaluateExpr(ctx, expression.right, binding)
-    else if (type === 'or')
-        return evaluateExpr(ctx, expression.left, binding) || evaluateExpr(ctx, expression.right, binding)
-    else if (type === 'not')
-        return !evaluateExpr(ctx, expression.expression, binding)
-    else if (type === 'clasped')
-        return evaluateExpr(ctx, expression.expression, binding)
-    else if (type === 'term')
-        return evaluateTerm(ctx, expression, binding)
-    else if (type === 'function')
-        return evaluateFunc(ctx, expression, binding)
-}
-
-var evaluateFunc = function (ctx, statement, binding) {
-    if (statement.name === 'isParent') {
-        var child = eval('binding["' + statement.params[1][0] + '"]' + (statement.params[1].length > 1 ? '.' + _.tail(statement.params[1]).join('.') : ''))
-        var parent = eval('binding["' + statement.params[0][0] + '"]' + (statement.params[0].length > 1 ? '.' + _.tail(statement.params[0]).join('.') : ''))
-        return child.indexOf(parent) !== -1
-    }
-}
-
-var evaluateTerm = function (ctx, term, binding) {
-    binding = binding || {}
-    binding.source = ctx.source
-    binding.origin = ctx.origin
-    binding.sourceType = ctx.sourceType
-    binding.originType = ctx.originType
-    binding.operation = ctx.operation
-    binding.parameters = ctx.parameters
-
-    var expr
-    if (term.field1)
-        expr = 'binding["' + term.field1[0] + '"]' + (term.field1.length > 1 ? '.' + _.tail(term.field1).join('.') : '') + ' ' + term.op +
-                ' ' + 'binding["' + term.field2[0] + '"]' + (term.field2.length > 1 ? '.' + _.tail(term.field2).join('.') : '')
-    else
-        expr = 'binding["' + term.field[0] + '"]' + (term.field.length > 1 ? '.' + _.tail(term.field).join('.') : '') + ' ' + term.op + ' ' + term.value
-    return eval(expr)
-}
-
 var monitor = function (temporalConstraint) {
 
     var self = this
@@ -90,7 +43,7 @@ var monitor = function (temporalConstraint) {
             var ctx = {}
             ctx[self.sequence[0]] = t
             ctx[link.id] = trace
-            if (evaluateExpr(trace, link.condition, ctx)) {
+            if (trace.evaluateExpr(link.condition, ctx)) {
                 found = true
                 return false
             }
@@ -102,7 +55,7 @@ var monitor = function (temporalConstraint) {
         var result
         /*  push each trace to the matching filters bag  */
         _.each(this.filters, function (filter) {
-            var res = evaluateExpr(trace, filter.condition)
+            var res = trace.evaluateExpr(filter.condition)
             if (res) {
                 var link = this.links[filter.id]
                 if (!predIsAvailable(filter.id) || (link && !checkLink(link, trace))) {
