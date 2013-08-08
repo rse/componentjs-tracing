f6f8c31b7d7a1fe0b136ed4f51f85fe1084bfe45
Christian Vaas <christianvaas@auspex.eu>
2013-08-08 17:01:20 +0200

    Turned the tree upside down

--- app/ui/app/app-ui-comp-component-tree.js
+++ app/ui/app/app-ui-comp-component-tree.js
@@ -13,18 +13,12 @@ app.ui.comp.componentTree = cs.clazz({
     mixin: [ cs.marker.controller ],
     protos: {
         create: function () {
+            cs(this).property('ComponentJS:state-auto-increase', true)
+
             cs(this).create('{model/view}',
                 app.ui.comp.componentTree.model,
                 app.ui.comp.componentTree.view
             )
-        },
-        prepare: function () {
-        },
-        render: function () {
-        },
-        release: function () {
-        },
-        cleanup: function () {
         }
     }
 })
@@ -35,10 +29,8 @@ app.ui.comp.componentTree.model = cs.clazz({
         create: function () {
         },
         prepare: function () {
-            var self = this
         },
         render: function () {
-            var self = this
         },
         release: function () {
         },
@@ -49,15 +41,24 @@ app.ui.comp.componentTree.model = cs.clazz({
 
 app.ui.comp.componentTree.view = cs.clazz({
     mixin: [ cs.marker.view ],
+    dynamics: {
+        idx: 0
+    },
     protos: {
-        create: function () {
-        },
-        prepare: function () {
-        },
         render: function () {
+            var self = this
             var content = $.markup("componentTree-content")
 
-            cs(this).plug(content)
+            cs(self).plug({
+                object: content,
+                spool: 'rendered'
+            })
+
+            $('#componentTree-content').click(function () { self.addAsChildByName(prompt('Add where?','/'), self.newNode(self.idx++)) })
+            //$('#componentTree-content').click(function () { self.removeByName(prompt('Delete which?','Library')) })
+        },
+        show: function () {
+            var self = this
 
             var treeData = {
                 name: "/",
@@ -123,178 +124,205 @@ app.ui.comp.componentTree.view = cs.clazz({
                         ]
                     }
                 ]
-            };
+            }
 
-            var newNode = {
-                name: "Döner",
+            self.newNode = function (idx) {
+                return {
+                name: "Döner" + idx,
                 contents: [
-                    { name: 'Chris' },
-                    { name: 'Uwe' },
-                    { name: 'Adrian'}
+                    { name: 'Chris' + idx },
+                    { name: 'Uwe' + idx },
+                    { name: 'Adrian' + idx}
                 ]
-            };
+            }}
+
+            self.newNode2 = {
+                name: "root"
+            }
 
             var containerName = "#componentTree-content"
 
-            var visit = function (parent, visitFn, childrenFn)
+            var visit = function (parent, visitFn)
             {
-                if (!parent) return;
-
-                visitFn(parent);
-
-                var children = childrenFn(parent);
-                if (children) {
-                    var count = children.length;
+                if (!parent) return
+                visitFn(parent)
+                if (parent.contents) {
+                    var count = parent.contents.length
                     for (var i = 0; i < count; i++) {
-                        visit(children[i], visitFn, childrenFn);
+                        visit(parent.contents[i], visitFn)
                     }
                 }
             }
 
-            var totalNodes = 0;
-            var maxLabelLength = 0;
-            visit(treeData, function(d)
-            {
-                totalNodes++;
-                maxLabelLength = Math.max(d.name.length, maxLabelLength);
-            }, function(d)
-            {
-                return d.contents && d.contents.length > 0 ? d.contents : null;
-            });
+            var maxLabelLength = 0
+            visit(treeData, function (d) {
+                maxLabelLength = Math.max(d.name.length, maxLabelLength)
+            })
 
             var options = $.extend({
-                    nodeRadius: 5, fontSize: 12
-                });
+                nodeRadius: 5, fontSize: 12
+            })
 
-            var size = { width:$(containerName).outerWidth(), height: $(containerName).outerHeight() }
+            var size = { width: $(containerName).outerWidth(), height: $(containerName).outerHeight() }
 
             var tree = d3.layout.tree()
                 .sort(null)
-                .size([size.height, size.width - maxLabelLength * options.fontSize])
-                .children(function(d)
+                .size([ size.width, size.height - maxLabelLength - 20 ])
+                .children(function (d)
                 {
-                    return (!d.contents || d.contents.length === 0) ? null : d.contents;
-                });
-
-            var nodes = tree.nodes(treeData);
-            var links = tree.links(nodes);
+                    return (!d.contents || d.contents.length === 0) ? null : d.contents
+                })
 
             var layoutRoot = d3.select(containerName)
-                .append("svg:svg").attr("width", size.width).attr("height", size.height)
-                .append("svg:g")
-                .attr("class", "container")
-                .attr("transform", "translate(" + maxLabelLength + ",0)");
+                .append('svg:svg').attr('width', size.width).attr('height', size.height)
+                .append('svg:g')
+                .attr('class', 'container')
+                .attr('transform', 'translate(0,' + maxLabelLength + ')')
 
-            var link = d3.svg.diagonal()
-                .projection(function(d)
-                {
-                    return [d.y, d.x];
-                });
+            var update = function (data) {
+                var nodes = tree.nodes(treeData)
+                var links = tree.links(nodes)
+                var margin = 20
+                var elbow = function (d) {
+                  return 'M' + d.source.x + ',' + (d.source.y + options.nodeRadius) +
+                        'V' + (d.source.y + margin) + 'H' + d.target.x +
+                        'V' + (d.target.y - options.nodeRadius )
+                }
 
-            layoutRoot.selectAll("path.link")
-                .data(links)
-                .enter()
-                .append("svg:path")
-                .attr("class", "link")
-                .attr("d", link);
+                var exitLinks = layoutRoot.selectAll('path')
+                    .data(links, function (d) { return d.source.name + '#' + d.target.name })
+                    .exit()
+                    //.transition()
+                    //.duration(400)
+                    .style('opacity', 0)
+                    .remove()
 
-            var nodeGroup = layoutRoot.selectAll("g.node")
-                .data(nodes)
-                .enter()
-                .append("svg:g")
-                .attr("class", "node")
-                .attr("transform", function(d)
-                {
-                    return "translate(" + d.y + "," + d.x + ")";
-                });
+                var currentLinks = layoutRoot.selectAll('path')
+                    .data(links, function (d) { return d.source.name + '#' + d.target.name })
+                    //.transition()
+                    //.delay(600)
+                    .attr('d', elbow)
 
-            nodeGroup.append("svg:circle")
-                .attr("class", "node-dot")
-                .attr("r", options.nodeRadius);
-
-            nodeGroup.append("svg:text")
-                .attr("text-anchor", function(d)
-                {
-                    return d.children ? "end" : "start";
-                })
-                .attr("dx", function(d)
-                {
-                    var gap = 2 * options.nodeRadius;
-                    return d.children ? -gap : gap;
-                })
-                .attr("dy", 3)
-                .text(function(d)
-                {
-                    return d.name;
-                });
-
-            var removeByName = function (name) {
-                var node = layoutRoot.selectAll("g.node").filter(function (d) { return d.name === name })
-                if (node.data().length !== 1)
-                    return
-                var children = node.data()[0].contents
-
-                _.each(children, function (child) {
-                    layoutRoot.selectAll("path.link").filter(function (d) { return d.source.name === name || d.target.name === name }).remove()
-                    removeByName(child.name)
-                })
-
-                node.remove()
-                nodes = _.filter(nodes, function (node) {
-                    return node.name !== name
-                })
-            }
-
-            var addAsChildByName = function (name, newNode) {
-                var node = layoutRoot.selectAll("g.node").filter(function (d) { return d.name === name })
-                if (node.data().length !== 1)
-                    return
-                treeData.contents.push(newNode)
-                nodes = tree.nodes(treeData)
-                links = tree.links(nodes)
-
-                layoutRoot.selectAll("path.link")
-                    .data(links)
+                var enterLinks = layoutRoot.selectAll('path.link')
+                    .data(links, function (d) { return d.source.name + '#' + d.target.name })
                     .enter()
-                    .append("svg:path")
-                    .attr("class", "link")
-                    .attr("d", link);
+                    .append('svg:path')
+                    //.transition()
+                    //.delay(700)
+                    .attr('class', 'link')
+                    .attr('d', elbow)
 
-                var grp = layoutRoot.selectAll("g.node")
-                    .data(nodes)
+                var exitNodes = layoutRoot.selectAll('g')
+                    .data(nodes, function (d) { return d.name })
+                    .exit()
+                    //.transition()
+                    //.duration(400)
+                    .style('opacity', 0)
+                    .remove()
+
+                var currentNodes = layoutRoot.selectAll('g')
+                    .data(nodes, function (d) { return d.name })
+                    //.transition()
+                    //.delay(600)
+                    .attr("transform", function (d)
+                        {
+                            return "translate(" + d.x + "," + d.y + ")"
+                        })
+
+                layoutRoot.selectAll('g')
+                    .select('text')
+                    .attr('text-anchor', function (d)
+                        {
+                            return d.contents ? 'start' : 'end'
+                        })
+                    .attr('dx', function (d)
+                        {
+                            var gap = 2 * options.nodeRadius
+                            return d.contents ? -gap : gap
+                        })
+                    .attr('dy', function (d) {
+                            return d.contents && d.parent ? -5 : 3
+                        })
+
+                var enterNodes = layoutRoot.selectAll('g')
+                    .data(nodes, function (d) { return d.name })
                     .enter()
                     .append('svg:g')
-                    .attr("transform", function(d)
-                    {
-                        return "translate(" + d.y + "," + d.x + ")";
-                    })
+                    .attr('class', 'node')
+                    .attr('transform', function (d)
+                        {
+                            return 'translate(' + d.x + ',' + d.y + ')'
+                        })
 
-                grp.append("svg:circle")
-                    .attr("class", "node-dot")
-                    .attr("r", options.nodeRadius)
+                enterNodes.append('svg:circle')
+                    //.transition()
+                    //.delay(700)
+                    .attr('class', 'node-dot')
+                    .attr('r', options.nodeRadius)
 
-                grp.append("svg:text")
-                    .attr("text-anchor", function(d)
-                    {
-                        return d.children ? "end" : "start";
-                    })
-                    .attr("dx", function(d)
-                    {
-                        var gap = 2 * options.nodeRadius;
-                        return d.children ? -gap : gap;
-                    })
-                    .attr("dy", 3)
+                enterNodes.append('svg:text')
+                    //.transition()
+                    //.delay(700)
+                    .attr('text-anchor', function (d)
+                        {
+                            return d.contents ? 'start' : 'end'
+                        })
+                    .attr('dx', function (d)
+                        {
+                            var gap = 2 * options.nodeRadius
+                            return d.contents ? gap : -gap
+                        })
+                    .attr('dy', function (d) {
+                            return d.contents && d.parent ? -5 : 3
+                        })
                     .text(function(d)
-                    {
-                        return d.name;
-                    });
+                        {
+                            return d.name
+                        })
             }
 
-            $('#componentTree-content').click(function () { removeByName('Developer'); addAsChildByName('opt', newNode) })
+            update(treeData)
+
+            var findInTree = function (tree, name) {
+                var acc = []
+                if (tree.name === name) {
+                    acc.push(tree)
+                }
+                _.each(tree.contents, function (content) {
+                    acc = acc.concat(findInTree(content, name))
+                })
+                return acc
+            }
+
+            var removeFromTree = function (tree, name) {
+                //  We want to remove the root node
+                if (tree.name === name)
+                    return {}
+                tree.contents = _.filter(tree.contents, function (content) { return content.name !== name })
+                _.each(tree.contents, function (content) { removeFromTree(content, name) })
+            }
+
+            self.removeByName = function (name) {
+                if (findInTree(treeData, name).length === 0)
+                    return
+                removeFromTree(treeData, name)
+                update(treeData)
+            }
+
+            self.addAsChildByName = function (name, newNode) {
+                var node = findInTree(treeData, name)[0]
+                /*  does the node exist?  */
+                if (!node || findInTree(treeData, newNode.name).length !== 0)
+                    return
+                if (!node.contents)
+                    node.contents = [ newNode ]
+                else
+                    node.contents.push(newNode)
+                update(treeData)
+            }
         },
         release: function () {
-        },
-        cleanup: function () {
+            cs(this).unspool('rendered')
         }
     }
 })
