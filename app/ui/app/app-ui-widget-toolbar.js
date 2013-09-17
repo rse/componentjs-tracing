37abd4a2f37f872c584b2c4fa18437ab8729b983
Christian Vaas <christianvaas@auspex.eu>
2013-09-17 13:57:32 +0200

    Introduced the non-managing widget pattern

--- app/ui/app/app-ui-widget-toolbar.js
+++ app/ui/app/app-ui-widget-toolbar.js
@@ -23,6 +23,49 @@ app.ui.widget.toolbar = cs.clazz({
                     cs(self, 'model').value('data:items', items)
                 }
             })
+        },
+        render: function () {
+            var self = this
+            cs(self, 'model').observe({
+                name: 'data:items', spool: 'materialized',
+                touch: true,
+                func: function (ev, nVal) {
+                    _.each(nVal, function (item, idx) {
+                        var cmp
+                        if (item.type === 'button') {
+                            if (!item.id)
+                                item.id = 'button-' + idx
+                            cmp = cs(self, 'model/view').create(item.id, app.ui.widget.toolbar.items.button)
+                            cmp.call('initialize', { label: item.label, icon: item.icon, stateClass: item.stateClass })
+                        }
+                        else if (item.type === 'input') {
+                            if (!item.id)
+                                item.id = 'input-' + idx
+                            cmp = cs(self, 'model/view').create(item.id, app.ui.widget.toolbar.items.input)
+                        }
+                        else if (item.type === 'text') {
+                            if (!item.id)
+                                item.id = 'text-' + idx
+                            cmp = cs(self, 'model/view').create(item.id, app.ui.widget.toolbar.items.text)
+                            cmp.call('initialize', { label: item.label, icon: item.icon })
+                        }
+                        else if (item.type === 'checkbox') {
+                            if (!item.id)
+                                item.id = 'checkbox-' + idx
+                            cmp = cs(self, 'model/view').create(item.id, app.ui.widget.toolbar.items.checkbox)
+                            cmp.call('initialize', { label: item.label, icon: item.icon })
+                        }
+                        if (item.pressedIcon)
+                            cmp.value('data:pressed-icon', item.pressedIcon)
+                    })
+                }
+            })
+        },
+        release: function () {
+            var self = this
+            _.each(cs(self, 'model').value('data:items'), function (cfg) {
+                cs(self, 'model/view/' + cfg.id).destroy()
+            })
         }
     }
 })
@@ -72,48 +115,6 @@ app.ui.widget.toolbar.view = cs.clazz({
 
             cs(self).socket({
                 ctx: $('.items', content)
-            })
-
-            cs(self).observe({
-                name: 'data:items', spool: 'materialized',
-                touch: true,
-                func: function (ev, nVal) {
-                    for (var i = 0; i < nVal.length; i++) {
-                        var item = nVal[i]
-                        var cmp
-                        if (item.type === 'button') {
-                            if (!item.id)
-                                item.id = 'button-' + i
-                            cmp = cs(self).create(item.id, app.ui.widget.toolbar.items.button)
-                            cmp.call('initialize', { label: item.label, icon: item.icon, stateClass: item.stateClass })
-                        }
-                        else if (item.type === 'input') {
-                            if (!item.id)
-                                item.id = 'input-' + i
-                            cmp = cs(self).create(item.id, app.ui.widget.toolbar.items.input)
-                        }
-                        else if (item.type === 'text') {
-                            if (!item.id)
-                                item.id = 'text-' + i
-                            cmp = cs(self).create(item.id, app.ui.widget.toolbar.items.text)
-                            cmp.call('initialize', { label: item.label, icon: item.icon })
-                        }
-                        else if (item.type === 'checkbox') {
-                            if (!item.id)
-                                item.id = 'checkbox-' + i
-                            cmp = cs(self).create(item.id, app.ui.widget.toolbar.items.checkbox)
-                            cmp.call('initialize', { label: item.label, icon: item.icon })
-                        }
-                        if (item.pressedIcon)
-                            cmp.value('data:pressed-icon', item.pressedIcon)
-                    }
-                }
-            })
-        },
-        release: function () {
-            var self = this
-            _.each(cs(self).value('data:items'), function (cfg) {
-                cs(self, cfg.id).destroy()
             })
         }
     }
